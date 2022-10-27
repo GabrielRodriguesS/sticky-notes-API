75f3cc1fb39f1328bc2cfc9aa963f7e42f7d52e7
Livia <alvesm.livia@gmail.com>
2022-10-26 21:44:44 -0300

    Criação dos repositórios.

---
+++ src/main/java/com/portifolio/stickyNotes/repository/NotasRepository.java
@@ -0,0 +1,10 @@
+package com.portifolio.stickyNotes.repository;
+
+import com.portifolio.stickyNotes.model.Notas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotasRepository extends JpaRepository<Notas, Integer> {
+}
+
